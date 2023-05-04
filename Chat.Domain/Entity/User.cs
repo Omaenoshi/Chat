311403641abab6fbac00540ade87bbd88d6c93c7
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2023-05-04 15:56:57 +0500

    refactor: rewrote constructor in User

--- Chat.Domain/Entity/User.cs
+++ Chat.Domain/Entity/User.cs
@@ -6,12 +6,21 @@ public class User
     public string Name { get; set; }
     public string Login { get; set; }
 
-    public ICollection<Room> Rooms { get; set; } = new List<Room>();
+    public ICollection<Room> Rooms { get; set; }
 
     public User(int id, string name, string login)
     {
         Id = id;
         Name = name;
         Login = login;
+        Rooms = new List<Room>();
+    }
+    
+    public User(int id, string name, string login, ICollection<Room> rooms)
+    {
+        Id = id;
+        Name = name;
+        Login = login;
+        Rooms = rooms;
     }
 }
